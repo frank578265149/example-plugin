a2f3f6a4b1ad5bd9cf90d0491bfcd66d97f7705f
frank <578265149@qq.com>
2017-03-30 13:57:00 +0800

    Initial commit

---
+++ src/main/java/org/elasticsearch/plugin/example/ExamplePlugin.java
@@ -0,0 +1,5 @@
+package org.elasticsearch.plugin.example;
+import org.elasticsearch.plugins.AbstractPlugin;
+public class ExamplePlugin extends AbstractPlugin { @Override public String name() { return "example-plugin"; }
+@Override public String description() { return "Example Plugin Description"; }
+}
