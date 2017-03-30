73943c9d5fb7503c7ba77e82b43f2fdea08d1384
frank <578265149@qq.com>
2017-03-30 16:57:34 +0800

    first

--- src/main/java/org/elasticsearch/plugin/example/ExamplePlugin.java
+++ src/main/java/org/elasticsearch/plugin/example/ExamplePlugin.java
@@ -1,5 +1,23 @@
 package org.elasticsearch.plugin.example;
-import org.elasticsearch.plugins.AbstractPlugin;
-public class ExamplePlugin extends AbstractPlugin { @Override public String name() { return "example-plugin"; }
-@Override public String description() { return "Example Plugin Description"; }
+
+import org.elasticsearch.common.collect.Lists; import org.elasticsearch.common.inject.Module; import org.elasticsearch.plugins.AbstractPlugin;
+import java.util.Collection;
+public class ExamplePlugin extends AbstractPlugin {
+
+
+    public String name() {
+        return "example-plugin";
+    }
+
+    public String description() {
+        return "Example Plugin Description";
+    }
+    public Collection<Class<? extends Module>> modules() {
+
+        Collection<Class<? extends Module>> modules = Lists.newArrayList();
+
+        modules.add(ExampleRestModule.class); return modules;
+
+    }
+
 }
