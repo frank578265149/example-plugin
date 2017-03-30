38b02d0e5be725fc41a710c04c9926af24a60fa4
frank <578265149@qq.com>
2017-03-30 16:56:39 +0800

    first

---
+++ src/main/java/org/elasticsearch/plugin/example/HelloRestHandler.java
@@ -0,0 +1,24 @@
+package org.elasticsearch.plugin.example;
+
+/**
+ * Created by frank on 17-3-30.
+ */
+import org.elasticsearch.rest.*;
+import org.elasticsearch.common.inject.Inject;
+import static org.elasticsearch.rest.RestRequest.Method.GET;
+import static org.elasticsearch.rest.RestStatus.OK;
+
+public class HelloRestHandler implements RestHandler {
+    @Inject
+    public HelloRestHandler(RestController restController) {
+        restController.registerHandler(GET, "/_hello", this);
+    }
+
+    public void handleRequest(final RestRequest request, final RestChannel channel) {
+        String who = request.param("who");
+        String whoSafe = (who!=null) ? who : "world";
+        channel.sendResponse(new BytesRestResponse(RestStatus.ACCEPTED));
+
+
+    }
+}
